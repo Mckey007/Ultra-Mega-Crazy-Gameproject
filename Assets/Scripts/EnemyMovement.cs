796b31ed76be5dbc670aec30997a1f5cc4368d97
Dominik Schulz <dominik.schulz@outlook.de>
2022-12-13 17:20:13 +0100

    movementAdjustments/created first Enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float direction = -1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move() {
+        if(rb.velocity.x == 0) {
+            this.direction = -this.direction;
+        }
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+         switch(collision.gameObject.tag) {
+            case "Player":
+                this.direction = -this.direction;
+                break;
+            default:
+                break;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
     private Rigidbody2D rb;
     [SerializeField]
     private float speed = 10f;
     [SerializeField]
     private float gravity = -9.8f;
     [SerializeField]
-    private float jumpHeight = 20f;
+    private float jumpHeight = 10f;
+
+    private bool isGrounded = false;
+    private bool isFreezed = false;
 
 
     void Start()
@@ -20,26 +25,90 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        float vertical = Input.GetAxis("Vertical"); // W = 1, S = -1
-        float horizontal = Input.GetAxis("Horizontal"); // A = -1 , D = 1
-        Vector3 moveDirection = new(horizontal, 0, 0);
-
-        // groundedCheck
-
-        if(true) { // grounded?
-            if(vertical > 0) // jump
-            {
-                moveDirection.y = jumpHeight;
-                // grounded = false;
+        //Freeze handle
+        if(this.isFreezed == true) {
+            if(Input.GetKey(KeyCode.Space)) {
+                reset();
             }
         }
-        else
-        {
-            moveDirection.y = gravity;
-        }
-        
 
-        // Todo FixedUpdate?
-        rb.MovePosition(transform.position + moveDirection * speed * Time.deltaTime);
+        //Reset handle
+        if(transform.position.y < -10f) {
+            reset();
+        }
+
+        //Movement horizontal
+        move();
+
+        //Jump
+        if(Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            jump();
+        }
+
+    }
+
+    void move() {
+        float horizontal = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    void jump() {
+        rb.velocity= new Vector2(rb.velocity.x, jumpHeight);
+    }
+
+    void onHit() {
+        Time.timeScale = 0;
+        this.isFreezed = true;
+    }
+
+    void reset() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {   
+        switch(collision.gameObject.tag) {
+            case "Ground":
+                isGrounded = true;
+                break;
+            case "Enemy":
+                onHit();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        switch(other.gameObject.tag) {
+            case "Ground":
+                isGrounded = false;
+                break;
+            default:
+                break;
+        }
     }
 }
+
+        ////DEPRECADED
+        //float vertical = Input.GetAxis("Vertical"); // W = 1, S = -1
+        //float horizontal = Input.GetAxis("Horizontal"); // A = -1 , D = 1
+        //Vector3 moveDirection = new(horizontal, 0, 0);
+        //// groundedCheck
+
+        //if(true) { // grounded?
+        //    if(vertical > 0) // jump
+        //    {
+        //        //moveDirection.y = jumpHeight;
+        //        // grounded = false;
+        //    }
+        //}
+        //else
+        //{
+        //    moveDirection.y = gravity;
+        //}
+        
+
+        //// Todo FixedUpdate?
+        //rb.MovePosition(transform.position + moveDirection * speed * Time.deltaTime);
